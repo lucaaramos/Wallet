1f3307ff512ffd8c843ea4247f84d2cfe1a59e9d
Luca Ramos <lucaramos99@gmail.com>
2026-04-22 00:49:56 -0300

    Implement RabbitMQ connection, publishing, and consumer functionality

--- shared/rabbit/message.go
+++ shared/rabbit/message.go
@@ -1,1 +1,7 @@
 package rabbit
+
+type Event struct {
+	EventID string      `json:"event_id"`
+	Type    string      `json:"type"`
+	Data    interface{} `json:"data"`
+}
--- shared/rabbit/publisher.go
+++ shared/rabbit/publisher.go
@@ -1,1 +1,38 @@
 package rabbit
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/google/uuid"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+var exchange = "wallet.events"
+
+func Publish(ctx context.Context, routingKey string, data interface{}) error {
+	ch := GetChannel()
+
+	event := Event{
+		EventID: uuid.New().String(),
+		Type:    routingKey,
+		Data:    data,
+	}
+
+	body, err := json.Marshal(event)
+	if err != nil {
+		return err
+	}
+
+	return ch.PublishWithContext(
+		ctx,
+		exchange,
+		routingKey,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "application/json",
+			Body:        body,
+		},
+	)
+}
